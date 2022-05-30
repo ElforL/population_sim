e3f2bd64ea58a5128b999b6d66b7227c03e197d7
Laith Shono <elfordev@gmail.com>
2022-05-30 21:39:21 +0300

    ✨ Extract widget and ability to pause with space

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:space_shooter/game/sim_game.dart';
 
 void main() async {
@@ -9,20 +10,54 @@ void main() async {
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
 
-  var gameWidget = GameWidget(
-    game: BlobsSim(
-      20,
-      20,
-    ),
+  final blobsSim = BlobsSim(
+    20,
+    20,
   );
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: gameWidget,
-        ),
+
+  var gameWidget = SimulatorWidget(game: blobsSim);
+
+  runApp(MaterialApp(
+    home: gameWidget,
+  ));
+}
+
+class SimulatorWidget extends StatefulWidget {
+  const SimulatorWidget({
+    Key? key,
+    required this.game,
+  }) : super(key: key);
+
+  final BlobsSim game;
+
+  @override
+  State<SimulatorWidget> createState() => _SimulatorWidgetState();
+}
+
+class _SimulatorWidgetState extends State<SimulatorWidget> {
+  void onKey(RawKeyEvent event) {
+    var isSpace = event.logicalKey == LogicalKeyboardKey.space;
+    final isPressed = event.isKeyPressed(LogicalKeyboardKey.space);
+    if (isSpace && !event.repeat && isPressed) {
+      setState(() {
+        widget.game.isRunning = !widget.game.isRunning;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RawKeyboardListener(
+      onKey: onKey,
+      focusNode: FocusNode(),
+      child: Stack(
+        children: [
+          GameWidget(
+            game: widget.game,
+          ),
+          if (!widget.game.isRunning) const Text('Paused'),
+        ],
       ),
-    ),
-  );
+    );
+  }
 }
